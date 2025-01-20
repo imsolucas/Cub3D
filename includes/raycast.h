e5463a9ca5be1b47187f560d34e06b8e20144d40
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-01-20 11:20:34 +0800

    Add DDA calculations

--- includes/raycast.h
+++ includes/raycast.h
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 15:29:17 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/01/16 18:58:52 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/01/20 11:19:34 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,35 @@
 
 # include "cub3d.h"
 
-typedef struct s_player t_player;
+typedef struct s_player	t_player;
 
 typedef struct s_ray
 {
-	double	camera_x;
-	double	dir_x;
-	double	dir_y;
-	double	side_dist_x;
-	double	side_dist_y;
-	double	delta_dist_x;
-	double	delta_dist_y;
-	double	perp_wall_dist;
-	double	wall_x;
-	int		map_x;
-	int		map_y;
-	int		step_x;
-	int		step_y;
-	int		hit;
-	int		side;
-	int		line_height;
-	int		draw_start;
-	int		draw_end;
-}			t_ray;
+	double				camera_x;
+	double				dir_x;
+	double				dir_y;
+	double				side_dist_x;
+	double				side_dist_y;
+	double				delta_dist_x;
+	double				delta_dist_y;
+	double				perp_wall_dist;
+	double				wall_x;
+	int					map_x;
+	int					map_y;
+	int					step_x;
+	int					step_y;
+	int					hit;
+	int					side;
+	int					line_height;
+	int					draw_start;
+	int					draw_end;
+}						t_ray;
 
-void	set_raycast(t_ray *ray, t_player *player, int x);
+// raycasting.c
+int						raycasting(t_game *game, t_ray *ray);
+void					init_raycast(t_ray *ray, t_player *player, int x);
+void					init_dda(t_ray *ray, t_player *player);
+void					start_dda(t_game *game, t_ray *ray);
+void					calc_line_height(t_ray *ray, t_player *player);
 
 #endif
--- src/render/raycasting.c
+++ src/render/raycasting.c
@@ -6,16 +6,44 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 15:07:52 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/01/16 19:01:38 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/01/20 11:19:46 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+int	raycasting(t_game *game, t_ray *ray)
+{
+	int		x;
+
+	x = 0;
+	while (x < WIN_WIDTH)
+	{
+		init_raycast(ray, &game->player, x);
+		init_dda(ray, &game->player);
+		start_dda(game, ray);
+		calc_line_height(ray, &game->player);
+		draw_line(game, ray, x);
+		x++;
+	}
+	return (0);
+}
+
 /**
  * camera_x: x-coordinate in camera space (range from -1 to 1)
+ *
+ * ray->dir_x and ray->dir_y: direction vector of the ray
+ * -ray->dir_x > 0 if ray points to the right
+ * -ray->dir_x < 0 if ray points to the left
+ * -ray->dir_y < 0 if ray points upwards
+ * -ray->dir_y > 0 if ray points downwards
+ *
+ * player's x and y typcasted to int to get the map square the player is in
+ *
+
+	* delta_dist_x and delta_dist_y: length of ray from one x or y-side to next x or y-side
  */
-void	set_raycast(t_ray *ray, t_player *player, int x)
+void	init_raycast(t_ray *ray, t_player *player, int x)
 {
 	ray->camera_x = 2 * x / (double)WIN_WIDTH - 1;
 	ray->dir_x = player->dir_x + player->plane_x * ray->camera_x;
@@ -24,4 +52,79 @@ void	set_raycast(t_ray *ray, t_player *player, int x)
 	ray->map_y = (int)player->y;
 	ray->delta_dist_x = fabs(1 / ray->dir_x);
 	ray->delta_dist_y = fabs(1 / ray->dir_y);
+}
+
+/**
+ * x increases as you move right
+ * y increases as you move down
+ */
+void	init_dda(t_ray *ray, t_player *player)
+{
+	if (ray->dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (player->x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - player->x) * ray->delta_dist_x;
+	}
+	if (ray->dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (player->y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - player->y) * ray->delta_dist_y;
+	}
+}
+
+/**
+ * In DDA, the shorter side_dist is chosen to move the ray to the next square
+ */
+void	start_dda(t_game *game, t_ray *ray)
+{
+	while (ray->hit == 0)
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->map_x += ray->step_x;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->map_y += ray->step_y;
+			ray->side = 1;
+		}
+		if (ray->map_x < 0 || ray->map_x >= game->map.width || ray->map_y < 0
+			|| ray->map_y >= game->map.height)
+			break ;
+		else if (game->map.map[ray->map_y][ray->map_x] == '1')
+			ray->hit = 1;
+	}
+}
+
+void	calc_line_height(t_ray *ray, t_player *player)
+{
+	if (ray->side == 0)
+		ray->perp_wall_dist = (ray->side_dist_x - ray->delta_dist_x);
+	else
+		ray->perp_wall_dist = (ray->side_dist_y - ray->delta_dist_y);
+	ray->line_height = (int)(WIN_HEIGHT / ray->perp_wall_dist);
+	ray->draw_start = -(ray->line_height) / 2 + WIN_HEIGHT / 2;
+	if (ray->draw_start < 0)
+		ray->draw_start = 0;
+	ray->draw_end = ray->line_height / 2 + WIN_HEIGHT / 2;
+	if (ray->draw_end >= WIN_HEIGHT)
+		ray->draw_end = WIN_HEIGHT - 1;
+	if (ray->hit == 0)
+		ray->wall_x = player->y + ray->perp_wall_dist * ray->dir_y;
+	else
+		ray->wall_x = player->x + ray->perp_wall_dist * ray->dir_x;
+	ray->wall_x -= floor(ray->wall_x);
 }
