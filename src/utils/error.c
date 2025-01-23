7f3c6e8f947ccefa0ccb3883693dec3819b083bd
Lucas <denglucasyijiN@gmail.com>
2025-01-23 13:51:51 +0800

    added right back and left

--- src/utils/error.c
+++ src/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: imsolucas <imsolucas@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 11:58:50 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/01/23 13:43:07 by imsolucas        ###   ########.fr       */
+/*   Updated: 2025/01/23 13:51:00 by imsolucas        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,11 +56,50 @@ void move_forward(t_game *game)
         game->player.y = new_y;
 }
 
+void move_backward(t_game *game)
+{
+	double new_x = game->player.x - game->player.dir_x * MOVE_SPEED;
+	double new_y = game->player.y - game->player.dir_y * MOVE_SPEED;
+	
+	if (game->map.map[(int)game->player.y][(int)new_x] != '1')
+		game->player.x = new_x;
+	if (game->map.map[(int)new_y][(int)game->player.x] != '1')
+		game->player.y = new_y;
+}
+
+void move_left(t_game *game)
+{
+	double new_x = game->player.x + game->player.dir_y * MOVE_SPEED;
+	double new_y = game->player.y - game->player.dir_x * MOVE_SPEED;
+	
+	if (game->map.map[(int)game->player.y][(int)new_x] != '1')
+		game->player.x = new_x;
+	if (game->map.map[(int)new_y][(int)game->player.x] != '1')
+		game->player.y = new_y;
+}
+
+void move_right(t_game *game)
+{
+	double new_x = game->player.x - game->player.dir_y * MOVE_SPEED;
+	double new_y = game->player.y + game->player.dir_x * MOVE_SPEED;
+	
+	if (game->map.map[(int)game->player.y][(int)new_x] != '1')
+		game->player.x = new_x;
+	if (game->map.map[(int)new_y][(int)game->player.x] != '1')
+		game->player.y = new_y;
+}
+
 int	key_hook(int keycode, t_game *game)
 {
 	if (keycode == ESC)
 		cleanup(game);
 	else if (keycode == W)
 		move_forward(game);
+	else if (keycode == S)
+		move_backward(game);
+	else if (keycode == A)
+		move_left(game);
+	else if (keycode == D)
+		move_right(game);
 	return (0);
 }
