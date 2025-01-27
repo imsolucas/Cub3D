9f5acc28e0338e0e1fd3e51282a241a932208e16
Lucas <denglucasyijiN@gmail.com>
2025-01-27 15:27:07 +0800

    fix validation for floodfil and invalid rgb

--- src/movement/direction.c
+++ src/movement/direction.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   direction.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: imsolucas <imsolucas@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:42:52 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/01/23 16:04:19 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/01/27 15:17:09 by imsolucas        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ void	move_forward(t_game *game)
 {
 	double	new_x;
 	double	new_y;
+	double	speed;
 
-	new_x = game->player.x + game->player.dir_x * MOVE_SPEED;
-	new_y = game->player.y + game->player.dir_y * MOVE_SPEED;
+	speed = MOVE_SPEED;
+	if (game->player.sprint)
+		speed = SPRINT_SPEED;
+	new_x = game->player.x + game->player.dir_x * speed;
+	new_y = game->player.y + game->player.dir_y * speed;
 	if (game->map.map[(int)game->player.y][(int)new_x] != '1')
 		game->player.x = new_x;
 	if (game->map.map[(int)new_y][(int)game->player.x] != '1')
@@ -29,9 +33,13 @@ void	move_backward(t_game *game)
 {
 	double	new_x;
 	double	new_y;
+	double	speed;
 
-	new_x = game->player.x - game->player.dir_x * MOVE_SPEED;
-	new_y = game->player.y - game->player.dir_y * MOVE_SPEED;
+	speed = MOVE_SPEED;
+	if (game->player.sprint)
+		speed = SPRINT_SPEED;
+	new_x = game->player.x - game->player.dir_x * speed;
+	new_y = game->player.y - game->player.dir_y * speed;
 	if (game->map.map[(int)game->player.y][(int)new_x] != '1')
 		game->player.x = new_x;
 	if (game->map.map[(int)new_y][(int)game->player.x] != '1')
@@ -42,9 +50,13 @@ void	move_left(t_game *game)
 {
 	double	new_x;
 	double	new_y;
+	double	speed;
 
-	new_x = game->player.x + game->player.dir_y * MOVE_SPEED;
-	new_y = game->player.y - game->player.dir_x * MOVE_SPEED;
+	speed = MOVE_SPEED;
+	if (game->player.sprint)
+		speed = SPRINT_SPEED;
+	new_x = game->player.x + game->player.dir_y * speed;
+	new_y = game->player.y - game->player.dir_x * speed;
 	if (game->map.map[(int)game->player.y][(int)new_x] != '1')
 		game->player.x = new_x;
 	if (game->map.map[(int)new_y][(int)game->player.x] != '1')
@@ -55,9 +67,13 @@ void	move_right(t_game *game)
 {
 	double	new_x;
 	double	new_y;
+	double	speed;
 
-	new_x = game->player.x - game->player.dir_y * MOVE_SPEED;
-	new_y = game->player.y + game->player.dir_x * MOVE_SPEED;
+	speed = MOVE_SPEED;
+	if (game->player.sprint)
+		speed = SPRINT_SPEED;
+	new_x = game->player.x - game->player.dir_y * speed;
+	new_y = game->player.y + game->player.dir_x * speed;
 	if (game->map.map[(int)game->player.y][(int)new_x] != '1')
 		game->player.x = new_x;
 	if (game->map.map[(int)new_y][(int)game->player.x] != '1')
