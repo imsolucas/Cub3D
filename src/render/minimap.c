ddb2aef07c38933d3f457caff8068c13bc4d4ae1
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-02-03 16:53:37 +0800

    Organize minimap files in bonus rule

--- src/render/minimap.c
+++ src/render/minimap.c
@@ -6,11 +6,20 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 12:22:57 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/02/03 16:34:59 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/02/03 16:39:59 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int	render_frame(t_game *game)
+{
+	draw_floor_ceiling(game);
+	raycasting(game, &game->ray);
+	draw_minimap(game);
+	mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
+	return (0);
+}
 
 void	draw_minimap(t_game *game)
 {
