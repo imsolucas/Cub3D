6d8510b40fc6473ac5c4a8a9594e906d3ed61f00
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-02-03 11:01:59 +0800

    Removed redundant ceiling and floor drawing

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 11:21:56 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/01/24 14:30:09 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/02/03 10:59:23 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,28 +42,6 @@ void	draw_floor_ceiling(t_game *game)
 	}
 }
 
-// Solid color testing
-
-// void draw_line(t_game *game, t_ray *ray, int x)
-// {
-//     int y = 0;
-//     int wall_color;
-//     if (ray->side == 0)
-//         wall_color = 0xFF0000;
-//     else
-//         wall_color = 0x880000;
-//     while (y < WIN_HEIGHT)
-//     {
-//         if (y < ray->draw_start)
-//             put_pixel(game, x, y, rgb_to_hex(game->ceiling));
-//         else if (y >= ray->draw_start && y <= ray->draw_end)
-//             put_pixel(game, x, y, wall_color);
-//         else
-//             put_pixel(game, x, y, rgb_to_hex(game->floor));
-//         y++;
-//     }
-// }
-
 void	set_texture(t_game *game, t_ray *ray)
 {
 	ray->tex_x = (int)(ray->wall_x * TEXTURE_WIDTH);
@@ -97,20 +75,35 @@ void	draw_line(t_game *game, t_ray *ray, int x)
 	set_texture(game, ray);
 	step = 1.0 * TEXTURE_HEIGHT / ray->line_height;
 	tex_pos = (ray->draw_start - WIN_HEIGHT / 2 + ray->line_height / 2) * step;
-	y = 0;
-	while (y < WIN_HEIGHT)
+	y = ray->draw_start;
+	while (y <= ray->draw_end)
 	{
-		if (y < ray->draw_start)
-			put_pixel(game, x, y, rgb_to_hex(game->ceiling));
-		else if (y <= ray->draw_end)
-		{
-			tex_y = (int)tex_pos & (TEXTURE_HEIGHT - 1);
-			tex_pos += step;
-			put_pixel(game, x, y, get_texture_color(ray->current_texture,
-					ray->tex_x, tex_y));
-		}
-		else
-			put_pixel(game, x, y, rgb_to_hex(game->floor));
+		tex_y = (int)tex_pos & (TEXTURE_HEIGHT - 1);
+		tex_pos += step;
+		put_pixel(game, x, y, get_texture_color(ray->current_texture,
+			ray->tex_x, tex_y));
 		y++;
 	}
 }
+
+// Solid color testing
+
+// void draw_line(t_game *game, t_ray *ray, int x)
+// {
+//     int y = 0;
+//     int wall_color;
+//     if (ray->side == 0)
+//         wall_color = 0xFF0000;
+//     else
+//         wall_color = 0x880000;
+//     while (y < WIN_HEIGHT)
+//     {
+//         if (y < ray->draw_start)
+//             put_pixel(game, x, y, rgb_to_hex(game->ceiling));
+//         else if (y >= ray->draw_start && y <= ray->draw_end)
+//             put_pixel(game, x, y, wall_color);
+//         else
+//             put_pixel(game, x, y, rgb_to_hex(game->floor));
+//         y++;
+//     }
+// }
